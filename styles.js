241ff4d2890370457a9b3a8ce38a2583c3bf4a44
othmane <othman-19@hotmail.fr>
2021-06-21 16:17:16 +0100

    add the align-self property to box style and set it to 'strech'

--- styles.js
+++ styles.js
@@ -2,28 +2,28 @@ import { Platform, StyleSheet, StatusBar } from "react-native";
 
 export default StyleSheet.create({
   container: {
-  flex: 1,
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "space-around",
-  backgroundColor: "ghostwhite",
-  ...Platform.select({
-    ios: { paddingTop: 20 },
-    android: { paddingTop: StatusBar.currentHeight }
-  })
-},
-box: {
-  width: 300,
-  height: 100,
-  justifyContent: "center",
-  alignItems: "center",
-  backgroundColor: "lightgray",
-  borderWidth: 1,
-  borderStyle: "dashed",
-  borderColor: "darkslategray"
-},
-boxText: {
-  color: "darkslategray",
-  fontWeight: "bold"
-}
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "ghostwhite",
+    alignItems: "center",
+    justifyContent: "space-around",
+    ...Platform.select({
+      ios: { paddingTop: 20 },
+      android: { paddingTop: StatusBar.currentHeight }
+    })
+  },
+  box: {
+    height: 100,
+    justifyContent: "center",
+    alignSelf: "stretch",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderColor: "darkslategray"
+  },
+  boxText: {
+    color: "darkslategray",
+    fontWeight: "bold"
+  }
 });

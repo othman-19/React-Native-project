c19eaef57c767c83f9b8b8276787b36d3db09406
othmane <othman-19@hotmail.fr>
2021-06-22 17:42:31 +0100

    create the Home and Settings screens and set nevigation between them in tha App.js

--- navigation/App.js
+++ navigation/App.js
@@ -1,44 +1,8 @@
-import React from "react";
-import { View, StatusBar } from "react-native";
-import styles from "./styles";
-import Row from "./Row";
-import Column from "./Column";
-import Box from "./Box";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import Home from "./components/Home";
+import Settings from "./components/Settings";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar hidden={false} />
-      <Row>
-        <Column>
-          <Box>#1</Box>
-          <Box>#2</Box>
-        </Column>
-        <Column>
-          <Box>#3</Box>
-          <Box>#4</Box>
-        </Column>
-      </Row>
-      <Row>
-        <Column>
-          <Box>#5</Box>
-          <Box>#6</Box>
-        </Column>
-        <Column>
-          <Box>#7</Box>
-          <Box>#8</Box>
-        </Column>
-      </Row>
-      <Row>
-        <Column>
-          <Box>#9</Box>
-          <Box>#10</Box>
-        </Column>
-        <Column>
-          <Box>#11</Box>
-          <Box>#12</Box>
-        </Column>
-      </Row>
-    </View>
-  );
-}
+export default createAppContainer(
+  createStackNavigator({ Home, Settings }, { initialRouteName: "Home" })
+);

2f36f407e57506c2a96ee600b735c203f4bc9e73
othmane <othman-19@hotmail.fr>
2021-06-22 10:12:46 +0100

    add flex-wrap to the container style, add margin, height and alignItems to Box component style

--- styles.js
+++ styles.js
@@ -4,19 +4,21 @@ export default StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row",
+    flexWrap: 'wrap',
     backgroundColor: "ghostwhite",
     alignItems: "center",
-    justifyContent: "space-around",
     ...Platform.select({
       ios: { paddingTop: 20 },
       android: { paddingTop: StatusBar.currentHeight }
     })
   },
   box: {
+    margin: 10,
+    height: 100,
     width: 100,
     justifyContent: "center",
+    alignItems: 'center',
     alignSelf: "stretch",
-    alignItems: "center",
     backgroundColor: "lightgray",
     borderWidth: 1,
     borderStyle: "dashed",

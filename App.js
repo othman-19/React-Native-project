f73292a323c1ea5fbd204c3d600a87ac64d5e6fa
othmane <othman-19@hotmail.fr>
2021-06-22 10:19:46 +0100

    add 10 Box components to App.js -displayed as a grid-

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StatusBar } from "react-native";
 import styles from "./styles";
 import Box from "./Box";
+
+const boxes = new Array(10).fill(null).map((v, i) => i + 1);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box>#1</Box>
-      <Box>#2</Box>
-      <Box>#3</Box>
+      <StatusBar hidden={false} />
+      {boxes.map(i => (
+        <Box key={i}>#{i}</Box>
+      ))}
     </View>
   );
 }

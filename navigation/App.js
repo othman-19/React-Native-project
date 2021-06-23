101b371890ea6128846600e3f11a62e197d1abf1
othmane <othman-19@hotmail.fr>
2021-06-23 09:45:01 +0100

    add the Details screen to th App navigation stack

--- navigation/App.js
+++ navigation/App.js
@@ -2,7 +2,8 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import Home from "./components/Home";
 import Settings from "./components/Settings";
+import Details from "./components/Details";
 
 export default createAppContainer(
-  createStackNavigator({ Home, Settings }, { initialRouteName: "Home" })
+  createStackNavigator({ Home, Settings, Details }, { initialRouteName: "Home" })
 );

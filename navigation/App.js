6bee6b4dc006336926b351ce10dabd5e3db97566
othmane <othman-19@hotmail.fr>
2021-06-23 11:30:40 +0100

    install react-navigation-tabs react-navigation-drawer and use them on the App component

--- navigation/App.js
+++ navigation/App.js
@@ -1,5 +1,8 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { Platform } from "react-native";
 import Home from "./components/Home";
 import Settings from "./components/Settings";
 import Details from "./components/Details";

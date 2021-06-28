a3eb8766a874d748fba6d0ad60a72c044e541dee
othmane <othman-19@hotmail.fr>
2021-06-28 14:12:05 +0100

    add styles

--- navigation-indicators/styles.js
+++ navigation-indicators/styles.js
@@ -1,31 +1,9 @@
-import { Platform, StyleSheet, StatusBar } from "react-native";
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    flexWrap: 'wrap',
-    backgroundColor: "ghostwhite",
-    alignItems: "center",
-    ...Platform.select({
-      ios: { paddingTop: 20 },
-      android: { paddingTop: StatusBar.currentHeight }
-    })
-  },
-  box: {
-    margin: 10,
-    height: 100,
-    width: 100,
     justifyContent: "center",
-    alignItems: 'center',
-    alignSelf: "stretch",
-    backgroundColor: "lightgray",
-    borderWidth: 1,
-    borderStyle: "dashed",
-    borderColor: "darkslategray"
-  },
-  boxText: {
-    color: "darkslategray",
-    fontWeight: "bold"
+    alignItems: "center"
   }
 });

7f50acc455a83e4542daeb56c88e14143061fc1c
othmane <othman-19@hotmail.fr>
2021-06-29 09:25:08 +0100

    Add WhereAmI component

--- geolocation/App.js
+++ geolocation/App.js
@@ -1,14 +1,47 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View } from "react-native";
 import styles from "./styles";
-import Box from "./components/Box";
 
-export default function App() {
+const API_KEY = "";
+const URL = `https://maps.google.com/maps/api/geocode/json?key=${API_KEY}&latlng=`;
+
+export default function WhereAmI() {
+  const [address, setAddress] = useState("loading...");
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitude] = useState();
+
+  useEffect(() => {
+    function setPosition({ coords: { latitude, longitude } }) {
+      setLongitude(longitude);
+      setLatitude(latitude);
+      fetch(`${URL}${latitude},${longitude}`)
+        .then(
+          (resp) => resp.json(),
+          (e) => console.error(e)
+        )
+        .then(({ results }) => {
+          setAddress(results[0].formatted_address);
+        });
+    }
+
+    navigator.geolocation.getCurrentPosition(setPosition);
+
+    let watcher = navigator.geolocation.watchPosition(
+      setPosition,
+      (err) => console.error(err),
+      { enableHighAccuracy: true }
+    );
+
+    return () => {
+      navigator.geolocation.clearWatch(watcher);
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <Box>#1</Box>
-      <Box>#2</Box>
-      <Box>#3</Box>
+      <Text style={styles.label}>Address: {address}</Text>
+      <Text style={styles.label}>Latitude: {latitude}</Text>
+      <Text style={styles.label}>Longitude: {longitude}</Text>
     </View>
   );
 }

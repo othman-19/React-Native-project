84b1a5598d5fb51abbcff9f57def853cc5063cca
othmane <othman-19@hotmail.fr>
2021-07-01 09:02:29 +0100

    fix import style typo

--- step-progress/components/ProgressBar.js
+++ step-progress/components/ProgressBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { View, Text } from "react-native";
-import styles from "./styles";
+import styles from "../styles";
 import { ProgressBarComponent, progressProps } from "./ProgressBarComponent";
 
 function ProgressLabel({ show, progress }) {

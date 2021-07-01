a0588fcbd19727875dd5320a36a67abe61f015e5
othmane <othman-19@hotmail.fr>
2021-07-01 09:03:40 +0100

    Add styles

--- step-progress/styles.js
+++ step-progress/styles.js
@@ -1,31 +1,32 @@
-import { Platform, StyleSheet, StatusBar } from "react-native";
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    flexWrap: 'wrap',
-    backgroundColor: "ghostwhite",
-    alignItems: "center",
-    ...Platform.select({
-      ios: { paddingTop: 20 },
-      android: { paddingTop: StatusBar.currentHeight }
-    })
-  },
-  box: {
-    margin: 10,
-    height: 100,
-    width: 100,
     justifyContent: "center",
-    alignItems: 'center',
-    alignSelf: "stretch",
-    backgroundColor: "lightgray",
-    borderWidth: 1,
-    borderStyle: "dashed",
-    borderColor: "darkslategray"
+    alignItems: "center",
+    backgroundColor: "ghostwhite"
   },
-  boxText: {
-    color: "darkslategray",
+
+  nav: {
+    backgroundColor: "lightgray"
+  },
+
+  title: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: "bold",
+    alignItems: "center"
+  },
+
+  content: {
+    fontSize: 23,
     fontWeight: "bold"
+  },
+
+  progress: {
+    width: 170,
+    alignItems: "center",
+    padding: 1
   }
 });

1ffc265687e1aeb6c2651bd2f9dacb130339b5eb
othmane <othman-19@hotmail.fr>
2021-07-03 11:16:39 +0100

    delete useless code lines

--- maps/App.js
+++ maps/App.js
@@ -9,11 +9,8 @@ export default function App() {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
         return;
       }
-
-      // let location = await Location.getCurrentPositionAsync({});
     })();
   }, []);
 

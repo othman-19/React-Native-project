ffc04e034dab9aad9d5ec0275c464ea5d125883a
othmane <othman-19@hotmail.fr>
2021-07-03 12:28:53 +0100

    no need to import styleSheet

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import styles from './styles'
 
 export default function App() {

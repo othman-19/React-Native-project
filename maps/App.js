02080360e7220d938cdd3180cecc3da39434aaa6
othmane <othman-19@hotmail.fr>
2021-07-03 10:55:32 +0100

    Set the application permissions to allow us get position for map, install nmp packages

--- maps/App.js
+++ maps/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { View } from "react-native";
 import MapView from "react-native-maps";
 import styles from "./styles";
+import * as Location from 'expo-location';
 
-export default () => (
-  <View style={styles.container}>
-    <MapView style={styles.mapView} showsUserLocation followUserLocation />
-  </View>
-);
+export default function App() {
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      // let location = await Location.getCurrentPositionAsync({});
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.mapView} showsUserLocation followUserLocation />
+    </View>
+  )
+
+}
+    

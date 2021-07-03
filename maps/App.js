d3408471051a712306a0805a66d6bfcafe63d5b3
othmane <othman-19@hotmail.fr>
2021-07-03 09:48:14 +0100

    Add MapView component in App.js, ad styles, install packages

--- maps/App.js
+++ maps/App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import MapView from "react-native-maps";
+import styles from "./styles";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => (
+  <View style={styles.container}>
+    <MapView style={styles.mapView} showsUserLocation followUserLocation />
+  </View>
+);

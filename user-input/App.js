ab26c70af882d57dbee1bb557a0e2778e95a349f
othmane <othman-19@hotmail.fr>
2021-07-04 11:28:40 +0100

    Create the CollectingTextInput comonent

--- user-input/App.js
+++ user-input/App.js
@@ -15,3 +15,32 @@ function Input(props) {
 Input.propTypes = {
   label: PropTypes.string
 };
+
+export default function CollectingTextInput() {
+  const [changedText, setChangedText] = useState("");
+  const [submittedText, setSubmittedText] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Input label="Basic Text Input:" />
+      <Input label="Password Input:" secureTextEntry />
+      <Input label="Return Key:" returnKeyType="search" />
+      <Input label="Placeholder Text:" placeholder="Search" />
+      <Input
+        label="Input Events:"
+        onChangeText={e => {
+          setChangedText(e);
+        }}
+        onSubmitEditing={e => {
+          setSubmittedText(e.nativeEvent.text);
+        }}
+        onFocus={() => {
+          setChangedText("");
+          setSubmittedText("");
+        }}
+      />
+      <Text>Changed: {changedText}</Text>
+      <Text>Submitted: {submittedText}</Text>
+    </View>
+  );
+}
